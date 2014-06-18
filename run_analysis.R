8208c0db23dede9255614743e5a52684d4621cdc
Lore <lors2404@gmail.com>
2014-06-17 23:57:35 -0500

    paso 5

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,7 @@
 getwd()
 setwd("/Users/loredp/Documents/Coursera/Cleaning/Project2")
 dir()
+rm(list=ls(all=T)) 
 
 ## The experiments have been carried out with a group of 30 volunteers
 ## The obtained dataset has been randomly partitioned into two sets
@@ -8,22 +9,21 @@ dir()
 ## 30% (9) was selected for generated the test data
 
 ## activity labels (1-6)
-labels<-read.table("./UCI HAR Dataset/activity_labels.txt", as.is = T)
+labels <- read.table("./UCI HAR Dataset/activity_labels.txt", as.is = T)
 
 ## features (561)
-
 features <- read.table("./UCI HAR Dataset/features.txt",as.is=T)
 features <- features[,2]
 
 ## test set
-x_test<-read.table("./UCI HAR Dataset/test/X_test.txt",as.is=T)
-y_test<-read.table("./UCI HAR Dataset/test/Y_test.txt",as.is=T)
-subject_test<-read.table("./UCI HAR Dataset/test/subject_test.txt",as.is=T)
+x_test <- read.table("./UCI HAR Dataset/test/X_test.txt",as.is=T)
+y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt",as.is=T)
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt",as.is=T)
 
 ## train set
-x_train<-read.table("./UCI HAR Dataset/train/X_train.txt",as.is=T)
-y_train<-read.table("./UCI HAR Dataset/train/Y_train.txt",as.is=T)
-subject_train<-read.table("./UCI HAR Dataset/train/subject_train.txt",as.is=T)
+x_train <- read.table("./UCI HAR Dataset/train/X_train.txt",as.is=T)
+y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt",as.is=T)
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt",as.is=T)
 
 ## test 
 test <- cbind(subject_test,y_test,x_test)
@@ -45,6 +45,15 @@ names(data) <- gsub("^t", "time", names(data))
 
 data$actId<-factor(x,labels=labels$V2)
 data<-data[,c(1,2,69,3:68)]
+data$extra <- paste(data$subject,data$activity,sep = "-")
+
+#Falta hacer el resumen
+library(reshape2)
+
+dataMelt <- melt(data,id=c("subject","activity","extra"),measure.vars=names(data)[3:68])
+dat <- dcast(dataMelt,extra~ variable,mean)
+
+
 
 
 
